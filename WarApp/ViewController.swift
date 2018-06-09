e14a9b4b948b787c30b822676e4e47f5811ff0a0
Marisa Wadsworth <marisawadsworth@marisas-air.hub>
2018-06-09 21:11:31 +1200

    Initial Commit

---
+++ WarApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WarApp
+//
+//  Created by Marisa Wadsworth on 9/06/18.
+//  Copyright © 2018 Marisa Wadsworth. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

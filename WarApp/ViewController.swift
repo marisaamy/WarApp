24b705b80af935bc00a9fa8b9a7405063f0854e7
Marisa Wadsworth <marisawadsworth@marisas-air.hub>
2018-06-27 12:58:24 +1200

    Changed the cards to random when the deal button is pressed

--- WarApp/ViewController.swift
+++ WarApp/ViewController.swift
@@ -10,6 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var leftImageView:UIImageView!
+    
+    @IBOutlet weak var rightImageView: UIImageView!
+    
+    @IBOutlet weak var leftScoreLabel: UILabel!
+    
+    @IBOutlet weak var rightScoreLabel: UILabel!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +30,19 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    
+    @IBAction func dealTapped(_ sender: Any) {
+        
+        let leftRandomNumber = arc4random_uniform(13) + 2
+        //print("left random number is: \(leftRandomNumber)")
+        
+        let rightRandomNumber = arc4random_uniform(13) + 2
+        //print("right random number is: \(rightRandomNumber)")
+        
+        leftImageView.image = UIImage(named: "card\(leftRandomNumber)")
+        
+        rightImageView.image = UIImage(named: "card\(rightRandomNumber)")
+    }
+    
 }
 
